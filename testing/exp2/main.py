86042c5aa12fd770d1b41f28ce6dcb937da2c87d
Luca <luca.haar@icloud.com>
2023-01-05 06:11:00 +1100

    img match func -- wrapped all code in a function and gave the option of either point mode or rectangle mode

--- testing/exp2/main.py
+++ testing/exp2/main.py
@@ -1,58 +1,82 @@
 import cv2 as cv
 import numpy as np
 
-# define imgs as variables 
-pvz = cv.imread('./imgRef/PvZ.png', cv.IMREAD_UNCHANGED)
-peaShooter = cv.imread('./imgRef/peaShooter.png', cv.IMREAD_UNCHANGED)
+# wrap all this code in a function
+def findImageMatches(baseImagePath, isolatedImagePath, thresholdVal, mode):
 
-# get dimenshions of Pea Shooter img
-peaShooterW = peaShooter.shape[1]
-peaShooterH = peaShooter.shape[0]
+    # define imgs as variables 
+    baseImage = cv.imread(baseImagePath, cv.IMREAD_UNCHANGED)
+    isolatedImage = cv.imread(isolatedImagePath, cv.IMREAD_UNCHANGED)
+    # save dimenshions of Pea Shooter img
+    isolatedImageW = isolatedImage.shape[1]
+    isolatedImageH = isolatedImage.shape[0]
 
-# match shovel img against pvz img to see if it can be found
-result = cv.matchTemplate(pvz, peaShooter, cv.TM_CCOEFF_NORMED)
+    # set a threshold for matching accuracy
+    threshold = thresholdVal
 
-# set a threshold for accuracy
-threshold = 0.7
-# get all coord possitions where it matches with this threshold
-locations = np.where(result >= threshold)
-# refine the locations array to just return x and y coordinates of each matched location
-locations = list(zip(*locations[::-1]))
+    # match isolatedImage against baseImage with 1 of the following methods
+    # TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
+    method = cv.TM_CCOEFF_NORMED
+    result = cv.matchTemplate(baseImage, isolatedImage, method)
+    
+    # Get all positions from the match result that exceed the threshold
+    locations = np.where(result >= threshold)
+    # refine the locations array to just return x and y coordinates of each matched location
+    locations = list(zip(*locations[::-1]))
 
-# create list of rectangles [x, y, w, h] (so they can be reduced) 
-rectangles = []
-for l in locations:
-    rect = [int(l[0]), int(l[1]), peaShooterW, peaShooterH]
-    # add rectangle to rectangles arr twice (since if there is only 1 rect at a poss it will eliminate it when grouping)
-    rectangles.append(rect)
-    rectangles.append(rect)
+    # create list of rectangles [x, y, w, h] (so they can be grouped together) 
+    rectangles = []
+    for l in locations:
+        rect = [int(l[0]), int(l[1]), isolatedImageW, isolatedImageH]
+        # Add every box to the list twice in order to retain single (non-overlapping) boxes
+        rectangles.append(rect)
+        rectangles.append(rect)
 
-# group rectangles that are close to each other where 3rd parameter controls how close together they must be to be grouped
-rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+    # group rectangles that are close to each other where 3rd parameter controls how close together they must be to be grouped
+    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+
+    points = []
+    if len(rectangles):
+        # set rect properties
+        lineColor = (0, 0, 255) # (B,G,R)
+        lineThickness = 2
+        lineType = cv.LINE_8
+        markerColor = (0, 255, 255) # (B,G,R)
+        markerType = cv.MARKER_CROSS
+        markerSize = 30
+        markerThickness = 2
+
+        # loop over each location
+        for (x, y, w, h) in rectangles:
+
+            if mode == "rectangles":
+                # determine rect pos
+                topLeft = (x, y)
+                bottomRight = (x + w, y + h)
+                # draw the box
+                cv.rectangle(baseImage, topLeft, bottomRight, lineColor, lineThickness, lineType)
+
+            elif mode == "points":
+                # get center of rect
+                centerX = x + int(w/2)
+                centerY = y + int(h/2)
+                # save points
+                points.append((centerX, centerY))
+                # draw the center point
+                cv.drawMarker(baseImage, (centerX, centerY), markerColor, markerType, markerSize, markerThickness)
+
+        if mode:
+            # display baseImage with matched data
+            cv.imshow("Matched Image", baseImage)
+            cv.waitKey()
+            # save the image
+            # cv.imwrite('result_click_point.jpg', haystack_img)
 
 
-if len(rectangles):
-    print("Found some Pea Shooters")
+    else:
+        print("Didn't find any matches")
 
-    # set rect properties
-    lineColor = (0, 0, 255) # (B,G,R)
-    lineThickness = 2
-    lineType = cv.LINE_8
 
-    # loop over each location
-    for (x, y, w, h) in rectangles:
-        # determine rect pos
-        topLeft = (x, y)
-        bottomRight = (x + w, y + h)
-        # get center of rect
-        centerX = x + int(w/2)
-        centerY = y + int(h/2)
-        # draw the box
-        cv.rectangle(pvz, topLeft, bottomRight, lineColor, lineThickness, lineType)
 
-    # draw rectangle where the is match is located
-    cv.imshow("Matched Image", pvz)
-    cv.waitKey()
-
-else:
-    print("Didn't find any pea shooters")
+points = findImageMatches("./imgRef/PvZ.png", "./imgRef/peaShooter.png", thresholdVal = 0.7, mode = "points")
+# points = findImageMatches("albion_turnip.jpg", "albion_farm.jpg", thresholdVal = 0.7, mode = "rectangles")
