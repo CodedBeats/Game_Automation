0cba0bc6ac056253687293f0851c442516a3b2b4
Luca <luca.haar@icloud.com>
2023-01-06 01:35:26 +1100

    move counter, scoped variables, code cleanup -- added a counter in the top left that increments every time a move is made (and resets on restart), moved any variable that was used in multiple functions or needed to be saved outside the functions to the top of the js file, removed console.logs() and added comments and just generally cleaned up the code

--- memoryMatchClone/script.js
+++ memoryMatchClone/script.js
@@ -1,24 +1,41 @@
+// set all global variables
+const wonBanner = document.getElementById("won")
+const restartButton = document.getElementById("restart")
+const moveCounter = document.getElementById("counter")
+let picked = 0
+let score = 0
+let moves = 0
+let pairs = []
+let tileImgs = []
 
 let checkMatch = (t1, t2) => {
     let matches = false
     if (t1.style.backgroundColor == t2.style.backgroundColor) {
         matches = true
     }
+    // return a bool to be assigned to a variable when calling the func
     return matches
 }
 
 let restartGame = () => {
+    // hide wonBanner
+    wonBanner.style.display = "none"
+
+    // bring back all imgs
     const unknowTileImgs = document.querySelectorAll("img")
     unknowTileImgs.forEach((img) => {
         img.style.display = "block"
     });
+
+    // reset moves made and change text
+    moves = 0
+    moveCounter.innerHTML = `Moves Made: ${moves}`
 }
 
-let picked = 0
-let score = 0
-let pairs = []
-let tileImgs = []
 let revealTile = (imgID, tileID) => {
+    moves += 1
+    moveCounter.innerHTML = `Moves Made: ${moves}`
+
     if (picked > 0) {
         // store 2nd selected tile in arrs
         pairs[1] = document.getElementById(tileID)
@@ -31,15 +48,17 @@ let revealTile = (imgID, tileID) => {
         let tilesMatch = checkMatch(pairs[0], pairs[1])
         if (tilesMatch) {
             console.log("These tiles matched")
+            // increment score when a pair is found
             score += 1
+            // check if all pairs have been found to indicate the end of the game
             if (score == 4) {
-                document.getElementById("won").style.display = "block"
-                document.getElementById("restart").style.display = "block"
+                wonBanner.style.display = "block"
+                restartButton.style.display = "block"
             }
         } else {
             console.log("These tiles didn't match")
+            // add delay so user can see both tiles together before reseting the images
             setTimeout(function() {
-                console.log('3s wait before reset')
                 // could use a for loop but it's just 2 lines anyway
                 tileImgs[0].style.display = "block"
                 tileImgs[1].style.display = "block"
